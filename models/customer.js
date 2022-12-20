807d377644a62fc1aea321ca1e340732da2f7143
amarupd <amarduttupadhyay@gmail.com>
2022-12-20 16:19:00 +0530

    com

--- models/customer.js
+++ models/customer.js
@@ -1,1 +1,20 @@
-const Sequelize=require("sequelize");
+const Sequelize = require("sequelize");
+const sequelize = require("../database/database");
+
+const Customer = sequelize.define("customer", {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    }
+
+})
