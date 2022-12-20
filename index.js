a961d5851accb8f7ceca7e31068684437c150f03
amarupd <amarduttupadhyay@gmail.com>
2022-12-20 16:54:08 +0530

    sdfasdf

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ Customer.hasMany(Order);
 sequelize
     .sync()
     .then((result) => {
+        return Customer.create({name:"chandler bing",email:"cb@gmail.com"})
         console.log(result);
+    }).then(customer=>{
+        console.log("first customer created",customer);
     })
     .catch((err) => {
         console.log(err);
