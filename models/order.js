eb54f7db26bad4d24f3b4ac97a6cae98e2b26502
amarupd <amarduttupadhyay@gmail.com>
2022-12-20 16:22:38 +0530

    fsdf

--- models/order.js
+++ models/order.js
@@ -1,1 +1,12 @@
 const Sequelize=require("sequelize");
+const Order=Sequelize.define("order",{
+    id:{
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+    },
+    total:{
+        
+    }
+})
