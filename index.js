fad5c64b9fe4d7ea0e6ae604ab56d09724613503
amarupd <amarduttupadhyay@gmail.com>
2022-12-21 14:38:49 +0530

    gdf

--- index.js
+++ index.js
@@ -3,24 +3,24 @@ const Customer = require("./models/customer");
 const Order = require("./models/order");
 
 Customer.hasMany(Order);
-let customerId=null;
+let customerId = null;
 sequelize
     .sync()
     .then((result) => {
-        return Customer.create({name:"amar dutt upadhyay",email:"amarduttupadhyay@gmail.com"})
+        return Customer.create({ name: "amar dutt upadhyay", email: "amarduttupadhyay@gmail.com" })
         console.log(result);
     })
-    .then(customer=>{
-        customerId=customer.id;
-        console.log("first customer created",customer);
-        return customer.createOrder({total:48});
+    .then(customer => {
+        customerId = customer.id;
+        console.log("first customer created", customer);
+        return customer.createOrder({ total: 48 });
     })
-    .then(order=>{
+    .then(order => {
         console.log("order is:", order);
-        return Order.findAll({where:customerId});
+        return Order.findAll({ where: customerId });
     })
-    .then(orders=>{
-        console.log("order belong to customer",orders);
+    .then(orders => {
+        console.log("order belong to customer", orders);
     })
     .catch((err) => {
         console.log(err);
